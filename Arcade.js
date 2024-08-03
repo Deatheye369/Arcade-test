05a58db9d1fce1f27ab80ad5cba91b7d320b00a5
Deatheye369 <165529476+Deatheye369@users.noreply.github.com>
2024-08-03 10:45:28 -0400

    linked HTMl and JS

--- Arcade.js
+++ Arcade.js
@@ -1,7 +1,11 @@
 //arrows = document.querySelectorAll(".arrow");
 console.log("let me type please");
 
+const handleClick = (event) =>{
+    const direction = event.target.getAttribute('id');
+    console.log(direction);
+}
+
 arrows.forEach((arrow)=>{
     arrow.addEventListener('click',handleClick)
-    
 })
