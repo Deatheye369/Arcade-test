91bbcf4e841b5d801fca976f50117d7b217e98c5
Deatheye369 <165529476+Deatheye369@users.noreply.github.com>
2024-08-03 11:06:46 -0400

    linked html and java script

--- Arcade.js
+++ Arcade.js
@@ -1,11 +1,11 @@
 //arrows = document.querySelectorAll(".arrow");
 console.log("let me type please");
 
-const handleClick = (event) =>{
-    const direction = event.target.getAttribute('id');
-    console.log(direction);
-}
+const handleClick = (event) => {
+  const direction = event.target.getAttribute("id");
+  console.log(direction);
+};
 
-arrows.forEach((arrow)=>{
-    arrow.addEventListener('click',handleClick)
-})
+arrows.forEach((arrow) => {
+  arrow.addEventListener("click", handleClick);
+});
